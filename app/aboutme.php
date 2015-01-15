694d6aed277ba25cace6f9fba7a77cfe5e095b6f
Nitesh-Mishra <nitesh.mishra143@gmail.com>
2015-01-15 10:21:59 +0530

    Added the files

---
+++ app/aboutme.php
@@ -0,0 +1,28 @@
+<div class="container">
+<div class="row">
+<div class="col-xs-1"></div>
+<div class="col-xs-3 pull-left">
+	<img src="images/nitesh.jpg" alt="..." class="img-responsive">
+ </div>
+ <div class="col-xs-7">
+ 
+ <div>
+ 	<div><h4><strong>About Me :</strong></h4></div><!-- end of panel heading -->
+ 	<hr/>
+ 	<strong><i>I'm Nitesh Mishra. I live in (Mhow)Indore, India. I'm pursuing Master of Computer Applications (expected June 2015) from International Institute of Professional Studies, Devi Ahilya University.
+ 	</i></strong><br/><br/>
+ 	<h4><strong>Current Status :</strong></h4><hr/>
+ 	<strong><i>I'm currently enrolled in 12th (i.e. last) semester at my college. I'm supposed to do internship (referred to as "industrial training" by my college) in this semester and I'm actively looking for job/internship oppurtunities in 2015.
+ 	</i></strong>
+ 	<br><br><br>
+ 	<strong><i>I'm already placed in Cognizant via campus recruitment.
+ 	</i></strong>
+
+ </div><!-- end of panel -->
+
+ </div><!-- end of col-xs-7 div -->
+ </div><!-- end of row div -->
+ 
+ 
+ 
+ </div>
